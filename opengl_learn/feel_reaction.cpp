73e55551b877f70391695b47eac490c5a1b92ed7
adalbert44 <adalbert.makarovych@yandex.ua>
2017-11-09 21:45:54 +0200

    змінені деякі текстури

--- opengl_learn/feel_reaction.cpp
+++ opengl_learn/feel_reaction.cpp
@@ -301,16 +301,16 @@ void feel_mouse_pressed(int button, int state)
                     if (reostat(object[i][j].f.tex))
                         x=min(x,WinWid-425);
 
-                    object_info=Figure(x,x+200,y,y+75,main_menu_background.tex,1.0);
+                    object_info=Figure(x,x+200,y,y+75,left_menu_background_tex,1.0);
 
                     object_delete=Button_do(Figure(x+200,x+275,y,y+75,delete_mode_tex,1.0),&object_delete_func,window_light,window_shade.tex);
                     object_rotate=Button_do(Figure(x+275,x+350,y,y+75,rotate_tex,1.0),&object_rotate_func,window_light,window_shade.tex);
 
                     if (key(object[i][j].f.tex))
-                        object_change_state=Button_do(Figure(x+350,x+425,y,y+75,rotate_tex,1.0),
+                        object_change_state=Button_do(Figure(x+350,x+425,y,y+75,change_tex,1.0),
                                                       &object_change_state_func,window_light,window_shade.tex);
                     if (reostat(object[i][j].f.tex))
-                        object_change_R=Button_do(Figure(x+350,x+425,y,y+75,rotate_tex,1.0),
+                        object_change_R=Button_do(Figure(x+350,x+425,y,y+75,change_tex,1.0),
                                                       &object_change_R_func,window_light,window_shade.tex);
 
                     choosen_object={i,j};
